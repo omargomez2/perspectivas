c98bad188a7ddfe1621d7f5a34c33b5c1c198735
Omar S. Gómez <omargomez2@gmail.com>
2023-01-10 18:40:38 -0500

    Update streamlit_app.py
    v6

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,34 @@ def init_connection():
 
 conn = init_connection()
 
+
+
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
+
+# Creating a cursor object using the cursor() method
+
+cursor = conn.cursor()
+
+# Executing an MYSQL function using the execute() method
+
+cursor.execute("select version()")
+
+# Fetch a single row using fetchone() method.
+
+data = cursor.fetchone()
+
+st.write("Connection established to: ", data)
+
+# Closing the connection
+
+conn.close()
+
+#def run_query(query):
+#    with conn.cursor() as cur:
+#        cur.execute(query)
+#        return cur.fetchall()
 
 #rows = run_query("SELECT * from perspectivas;")
 
