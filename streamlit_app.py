0c5a090382968f930bcbc514a5a2bf9b34e7509a
Omar S. Gómez <omargomez2@gmail.com>
2023-01-10 18:44:58 -0500

    Update streamlit_app.py
    v7

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,35 +3,28 @@ import streamlit as st
 import pandas
 import psycopg2
 
-
 @st.experimental_singleton
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
 conn = init_connection()
 
-
-
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
 
 # Creating a cursor object using the cursor() method
-
-cursor = conn.cursor()
+cur=conn.cursor()
 
 # Executing an MYSQL function using the execute() method
-
-cursor.execute("select version()")
+cur.execute("select version()")
 
 # Fetch a single row using fetchone() method.
-
-data = cursor.fetchone()
+data = cur.fetchone()
 
 st.write("Connection established to: ", data)
 
 # Closing the connection
-
 conn.close()
 
 #def run_query(query):
