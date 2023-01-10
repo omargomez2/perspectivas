41bf098bad61e39e8651df691e11bd4d40ba4d5b
Omar S. Gómez <omargomez2@gmail.com>
2023-01-10 18:56:11 -0500

    Update streamlit_app.py
    tt

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,33 +4,20 @@ import pandas
 import psycopg2
 
 
-conn = psycopg2.connect(**st.secrets["postgres"])
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
 
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
 @st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
 
-# Creating a cursor object using the cursor() method
-cur = conn.cursor()
-
-# Executing an MYSQL function using the execute() method
-cur.execute("select version()")
-
-# Fetch a single row using fetchone() method.
-data = cur.fetchone()
-
-st.write("Connection established to: ", data)
-
-# Closing the connection
-conn.close()
-
-#def run_query(query):
-#    with conn.cursor() as cur:
-#        cur.execute(query)
-#        return cur.fetchall()
-
-#rows = run_query("SELECT * from perspectivas;")
-
-# Print results.
-##for row in rows:
-##    st.write(f"{row[0]} has a :{row[1]}:")
+rows = run_query("SELECT * from postgre_capleftus.stuff.perspectivas;")
