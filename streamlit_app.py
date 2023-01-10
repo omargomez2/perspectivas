7ec6aacb7694e2d44655eae89132ce1913188b0f
Omar S. Gómez <omargomez2@gmail.com>
2023-01-10 18:04:24 -0500

    Update streamlit_app.py
    v2

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,24 @@
 import streamlit
 import pandas
 import psycopg2
+
+
+@st.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from perspectivas;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
