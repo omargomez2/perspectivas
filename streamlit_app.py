f957a6969f1b14c83fac4e3d8066d394b852a5e0
Omar S. Gómez <omargomez2@gmail.com>
2023-01-10 18:35:48 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ def run_query(query):
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT * from perspectivas;")
+#rows = run_query("SELECT * from perspectivas;")
 
 # Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+##for row in rows:
+##    st.write(f"{row[0]} has a :{row[1]}:")
