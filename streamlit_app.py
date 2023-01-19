7b34edd5d14b3fd43f955aa8f22081b6b75e0356
Omar S. Gómez <omargomez2@gmail.com>
2023-01-19 13:37:37 -0500

    Update streamlit_app.py
    size

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,9 @@ rows = run_query("SELECT paper, titulo, enviado, autores, estado, notas from pos
 
 #index_labels=['Paper ID', 'Título', 'Enviado','Autores','Estado','Notas']
 
-dfp = pandas.DataFrame(rows, columns = ['Paper Id' , 'Título' , 'Enviado' , 'Autores' , 'Estado' , 'Notas'])
+streamlit.title('Journal status')
+
+dfp = pandas.DataFrame(rows, columns = ['Paper Id' , 'Título' , 'Enviado' , 'Autores' , 'Estado' , 'Notas'], width=1240, height=800)
 
 st.dataframe(dfp)
 
