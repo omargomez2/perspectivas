17394f9fd78f895553152e80632172d001f16aff
Omar S. Gómez <omargomez2@gmail.com>
2023-01-19 13:41:48 -0500

    Update streamlit_app.py
    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,6 @@ st.title('Journal status')
 
 dfp = pandas.DataFrame(rows, columns = ['Paper Id' , 'Título' , 'Enviado' , 'Autores' , 'Estado' , 'Notas'])
 
-st.dataframe(dfp, width=1940, height=600)
+st.dataframe(dfp, 1940, 540)
 
 conn.close()
