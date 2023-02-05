3de9d33afa260e1f83df73a548a3d5e2aa02e706
omargomez2@gmail.com <omargomez2@gmail.com>
2023-02-05 13:06:42 -0500

    envios

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,4 +43,9 @@ dfp.Días = dfp.Días.round().astype(int)
 
 st.dataframe(dfp, 1440, 540)
 
+
+rows_env = run_query("select DATE_PART('year', \"Fecha de envío\"::date) as año, count(*) as envíos from articles_rp ar group by año;")
+df_envios = pandas.DataFrame(rows_env)
+st.dataframe(df_envios)
+
 conn.close()
