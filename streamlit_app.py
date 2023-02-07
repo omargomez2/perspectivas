2fbdfa6a3ee53ff2f43fb340d538a0491561d20a
omargomez2@gmail.com <omargomez2@gmail.com>
2023-02-06 23:00:22 -0500

    j

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,16 +28,16 @@ def run_query(query):
 st.title('Revista Perspectivas')
 
 rows_aux = run_query("select * from estado_d;")
-df_auxx = pandas.DataFrame(rows_aux, columns = ['Año' , 'Envíos', 'En revisión','Publicados','Rechazados', 'En producción', 'Tasa', 'Delta env','Delta tasa'])
+df_aux = pandas.DataFrame(rows_aux, columns = ['Año' , 'Envíos', 'En revisión','Publicados','Rechazados', 'En producción', 'Tasa', 'Delta env','Delta tasa'])
 
 #---- Envíos
 col1, col2, col3, col4, col5, col6 = st.columns(6)
-col1.metric("Envíos 2018", round(df_auxx.loc[0].at["Envíos"]), df_auxx.loc[0].at["Delta env"])
-col2.metric("Envíos 2019", round(df_auxx.loc[1].at["Envíos"]), round(df_auxx.loc[1].at["Delta env"]))
-col3.metric("Envíos 2020", round(df_auxx.loc[2].at["Envíos"]), round(df_auxx.loc[2].at["Delta env"]))
-col4.metric("Envíos 2021", round(df_auxx.loc[3].at["Envíos"]), round(df_auxx.loc[3].at["Delta env"]))
-col5.metric("Envíos 2022", round(df_auxx.loc[4].at["Envíos"]), round(df_auxx.loc[4].at["Delta env"]))
-col6.metric("Envíos 2023", round(df_auxx.loc[5].at["Envíos"]), round(df_auxx.loc[5].at["Delta env"]))
+col1.metric("Envíos 2018", round(df_aux.loc[0].at["Envíos"]), df_aux.loc[0].at["Delta env"])
+col2.metric("Envíos 2019", round(df_aux.loc[1].at["Envíos"]), round(df_aux.loc[1].at["Delta env"]))
+col3.metric("Envíos 2020", round(df_aux.loc[2].at["Envíos"]), round(df_aux.loc[2].at["Delta env"]))
+col4.metric("Envíos 2021", round(df_aux.loc[3].at["Envíos"]), round(df_aux.loc[3].at["Delta env"]))
+col5.metric("Envíos 2022", round(df_aux.loc[4].at["Envíos"]), round(df_aux.loc[4].at["Delta env"]))
+col6.metric("Envíos 2023", round(df_aux.loc[5].at["Envíos"]), round(df_aux.loc[5].at["Delta env"]))
 
 
 #--- Manuscritos activos
@@ -53,7 +53,7 @@ st.dataframe(dfp, 1440, 540)
 
 
 #---- Gráfico de enviados
-df_envios = df_auxx.drop(['En revisión','Publicados','Rechazados', 'En producción', 'Tasa', 'Delta env','Delta tasa'], axis=1)
+df_envios = df_aux.drop(['En revisión','Publicados','Rechazados', 'En producción', 'Tasa', 'Delta env','Delta tasa'], axis=1)
 #df_envios.Año = df_envios.Año.round().astype(int)
 df_envios = df_envios.set_index('Año')
 
@@ -63,7 +63,7 @@ st.bar_chart(df_envios)
 
 
 #---Gráfico de estado
-df_estado = df_auxx.drop(['En revisión', 'En producción', 'Tasa', 'Delta env','Delta tasa'], axis=1)
+df_estado = df_aux.drop(['En revisión', 'En producción', 'Tasa', 'Delta env','Delta tasa'], axis=1)
 df_estado = df_estado.set_index('Año')
 
 st.header('Estado por año')
@@ -71,14 +71,14 @@ st.bar_chart(df_estado)
 #st.line_chart(df_estado)
 
 
-
+#---Métrica tasa de aceptación
 st.header('Tasa de aceptación por año')
 col7, col8, col9, col10, col11, col12 = st.columns(6)
-#col7.metric("Tasa A. 2018", df_aux.loc[0].at["Tasa"], "0")
-col8.metric("2019", str(round(df_auxx.loc[1].at["Tasa"]))+'%', df_auxx.loc[1].at["Delta tasa"])
-col9.metric("2020", str(round(df_auxx.loc[2].at["Tasa"]))+'%', str(round(df_auxx.loc[2].at["Delta tasa"]))+'%')
-col10.metric("2021", str(round(df_auxx.loc[3].at["Tasa"]))+'%', str(round(df_auxx.loc[3].at["Delta tasa"]))+'%')
-col11.metric("2022", str(round(df_auxx.loc[4].at["Tasa"]))+'%', str(round(df_auxx.loc[4].at["Delta tasa"]))+'%')
+col7.metric("Tasa A. 2018", df_aux.loc[0].at["Tasa"], df_aux.loc[1].at["Delta tasa"])
+col8.metric("2019", str(round(df_aux.loc[1].at["Tasa"]))+'%', df_aux.loc[1].at["Delta tasa"])
+col9.metric("2020", str(round(df_aux.loc[2].at["Tasa"]))+'%', str(round(df_aux.loc[2].at["Delta tasa"]))+'%')
+col10.metric("2021", str(round(df_aux.loc[3].at["Tasa"]))+'%', str(round(df_aux.loc[3].at["Delta tasa"]))+'%')
+col11.metric("2022", str(round(df_aux.loc[4].at["Tasa"]))+'%', str(round(df_aux.loc[4].at["Delta tasa"]))+'%')
 #col12.metric("Tasa A. 2023", df_aux.loc[5].at["Tasa"], "0")
 
 
