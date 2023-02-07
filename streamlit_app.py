36b61031d364c14ff8d1712a1b2ba770c7d21acc
omargomez2@gmail.com <omargomez2@gmail.com>
2023-02-06 21:46:39 -0500

    e

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ rows_env_delta = run_query("SELECT * from envíos_delta;")
 df_env_delta = pandas.DataFrame(rows_env_delta, columns = ['Año' , 'Envíos', 'Delta'])
 
 col1, col2, col3, col4, col5, col6 = st.columns(6)
-col1.metric("Envíos 2018", round(df_env_delta.loc[0].at["Envíos"]), round(df_env_delta.loc[0].at["Delta"]))
+col1.metric("Envíos 2018", round(df_env_delta.loc[0].at["Envíos"]), df_env_delta.loc[0].at["Delta"])
 col2.metric("Envíos 2019", round(df_env_delta.loc[1].at["Envíos"]), round(df_env_delta.loc[1].at["Delta"]))
 col3.metric("Envíos 2020", round(df_env_delta.loc[2].at["Envíos"]), round(df_env_delta.loc[2].at["Delta"]))
 col4.metric("Envíos 2021", round(df_env_delta.loc[3].at["Envíos"]), round(df_env_delta.loc[3].at["Delta"]))
