4a0b1ad35972cf41fa1b1fd38f8dc5c0a88272bf
omargomez2@gmail.com <omargomez2@gmail.com>
2023-02-07 00:02:18 -0500

    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,8 +91,9 @@ col11.metric("2022", str(round(df_aux.loc[4].at["Tasa"]))+'%', str(round(df_aux.
 st.header('Nube de palabras según títulos de envíos activos')
 
 text = ' '.join(dfp['Título'])
+new_string = ' '.join([w for w in text.split() if len(w)>3])
 #st.write(text)
-wordcloud = WordCloud().generate(text)
+wordcloud = WordCloud().generate(new_string)
 
 # Display the generated image:
 fig, ax = plt.subplots()
